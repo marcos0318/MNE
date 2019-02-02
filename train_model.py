3c7fc60bf6f7e62bf26dc9ad77544a3bc95a52e3
Jiaxin_Bai <jbai0318@gmail.com>
2019-02-02 22:41:25 +0800

    lol

--- train_model.py
+++ train_model.py
@@ -4,8 +4,7 @@
 from MRWE import *
 import sys
 
-
-# file_name = sys.argv[1]
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 file_name = "data/small_example.txt"
 edge_data_by_type, all_edges, all_nodes = load_network_data(file_name)
 model = train_model(edge_data_by_type)
